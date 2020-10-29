1177fd1fee5905b2f89b5be796cb30a482bb660b
Manpreet1434 <mannav1434@gmail.com>
2020-10-30 00:09:06 +0530

    Initial commit

---
+++ Assets/_Scripts/WorldIntrection.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldIntrection : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0) && UnityEngine.EventSystems.EventSystem.current.IsPointerOverGameObject()) {
+			GetInteraction();
+		}
+			
+	}
+
+	void GetInteraction(){
+
+		Ray interactionray = Camera.main.ScreenPointToRay (Input.mousePosition);
+
+		RaycastHit interactInfo;
+
+		if(Physics.Raycast(interactionray,out interactInfo,Mathf.Infinity)){
+			GameObject interactedObj = interactInfo.collider.gameObject;
+			if (interactedObj.tag == "interactable object") {
+				Debug.Log ("Interaction");
+			} else {
+
+			}
+		}
+	}
+
+
+}
